58e701e779c9bd64ab2a0f1c1ce8f61a8cd485ae
carolina <ci.arayagonzalez@gmail.com>
2024-11-05 00:23:05 -0300

    refac-peticiÃones http

---
+++ api/main.js
@@ -0,0 +1,154 @@
+import { readFile, writeFile } from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "../data/animes.json");
+
+const readFileData = async () => {
+  const data = await readFile(filePath, "utf-8");
+  const animesData = JSON.parse(data);
+  return animesData;
+};
+
+export const getAnimes = async (id, res) => {
+  try {
+    const listaAnimes = await readFileData();
+    const updateAnimesData = { ...listaAnimes };
+    if (!id) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({
+          message: "Lista actual de animes",
+          data: updateAnimesData,
+        })
+      );
+    } else {
+      if (updateAnimesData[id]) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.end(
+          JSON.stringify({
+            message: "Obtención de anime éxitosa",
+            data: updateAnimesData[id],
+          })
+        );
+      } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        return res.end(JSON.stringify({ message: "ID no encontrado" }));
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const setAnimes = (req, res) => {
+  try {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", async () => {
+      body = JSON.parse(body);
+
+      const listaAnimes = await readFileData();
+      const updateAnimesData = { ...listaAnimes };
+
+      const newAnime = {
+        nombre: body.nombre,
+        genero: body.genero,
+        año: body.año,
+        autor: body.autor,
+      };
+
+      //clave unica dinamica para cada objeto
+      const ultimoId = Math.max(...Object.keys(updateAnimesData).map(Number));
+      const nuevoId = (ultimoId + 1).toString();
+
+      updateAnimesData[nuevoId] = newAnime;
+      await writeFile(
+        filePath,
+        JSON.stringify(updateAnimesData, null, 2),
+        "utf-8"
+      );
+      res.writeHead(201, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({ message: "Registro éxitoso", data: newAnime })
+      );
+    });
+  } catch (err) {
+    console.error("Hubo un error en la solicitud", err);
+  }
+};
+
+export const updateAnimes = (id, req, res) => {
+  try {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", async () => {
+      body = JSON.parse(body);
+
+      const listaAnimes = await readFileData();
+      const updateAnimesData = { ...listaAnimes };
+
+      const idAnimeContent = Object.keys(updateAnimesData).map(Number);
+      const busqueda = idAnimeContent.find((anime) => anime == id);
+
+      if (busqueda) {
+        //validación de llave - valor del objeto actualizado
+        body = Object.fromEntries(
+          Object.entries(body).filter(
+            ([keyValue]) => keyValue in updateAnimesData[busqueda]
+          )
+        );
+
+        //validación en modificación (falta)
+
+        updateAnimesData[busqueda] = { ...updateAnimesData[busqueda], ...body };
+        await writeFile(
+          filePath,
+          JSON.stringify(updateAnimesData, null, 2),
+          "utf-8"
+        );
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.end(
+          JSON.stringify({ message: "Anime modificado", updateAnimesData })
+        );
+      } else {
+        res.writeHead(409, { "Content-Type": "application/json" });
+        return res.end(
+          JSON.stringify({ message: "Id no encontrado para mdificación" })
+        );
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteAnimes = async (id, res) => {
+  try {
+    const listaAnimes = await readFileData();
+    const updateAnimesData = { ...listaAnimes };
+    delete updateAnimesData[id];
+
+    const newAnimesData = Object.values(updateAnimesData).reduce(
+      (acc, item, index) => {
+        acc[index + 1] = item;
+        return acc;
+      },
+      {}
+    );
+
+    await writeFile(filePath, JSON.stringify(newAnimesData, null, 2), "utf-8");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(
+      JSON.stringify({ message: "Anime eliminado ", newAnimesData })
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
--- index.js
+++ index.js
@@ -1,101 +1,30 @@
 import http from "http";
 import url from "url";
-import { readFileSync } from "fs";
+import { deleteAnimes, getAnimes, setAnimes, updateAnimes } from "./api/main.js";
 
 const port = 3000;
 
+// Prueba de servidor funcionando (falta)
 http
   .createServer((request, response) => {
     const method = request.method;
     const urlParsed = url.parse(request.url, true);
     const pathName = urlParsed.pathname;
+    const id_anime = urlParsed.query.id;
 
-    // Petición GET - Todos animes && Anime por Id
     if (pathName == "/animes" && method == "GET") {
-      const id_anime = urlParsed.query.id;
-
-      if (!id_anime) {
-        try {
-          const data = readFileSync("./data/animes.json", "utf-8");
-          const animesData = JSON.parse(data);
-          response.writeHead(200, { "Content-Type": "application/json" });
-          response.end(
-            JSON.stringify({
-              message: "Lista actual de animes",
-              data: animesData,
-            })
-          );
-        } catch (err) {
-          response.writeHead(500, { "Content-Type": "application/json" });
-          response.end(
-            JSON.stringify({ message: "Error al cargar datos de animes" })
-          );
-          console.log(err);
-        }
-      } else {
-        try {
-          const data = readFileSync("./data/animes.json", "utf-8");
-          const animesData = JSON.parse(data);
-
-          if (animesData[id_anime]) {
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.end(
-              JSON.stringify({
-                message: "Obtención de anime éxitosa",
-                data: animesData[id_anime],
-              })
-            );
-          } else {
-            response.writeHead(404, { "Content-Type": "application/json" });
-            response.end(JSON.stringify({ message: "Anime no encontrado" }));
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      }
+      getAnimes(id_anime, response);
     }
 
     if (pathName == "/registrar-anime" && method == "POST") {
-      try {
-        let body = "";
-        request.on("data", (chunk) => {
-          body += chunk.toString();
-        });
-
-        request.on("end", () => {
-          body = JSON.parse(body);
-
-          const contentJson = fs.readFileSync("./data/animes.json", "utf-8");
-          const contentJS = JSON.parse(contentJson);
-
-          const newAnime = {
-            nombre: body.nombre,
-            genero: body.genero,
-            año: body.año,
-            autor: body.autor,
-          };
-
-          //clave unica dinamica para objeto .json
-          const ultimoId = Math.max(...Object.keys(contentJS).map(Number));
-          const nuevoId = (ultimoId + 1).toString();
-
-          contentJS[nuevoId] = newAnime;
-          fs.writeFileSync(
-            "./data/animes.json",
-            JSON.stringify(contentJS, null, 2),
-            "utf-8"
-          );
-        });
-      } catch (err) {
-        console.error("Hubo un error en la solicitud", err);
-      }
-      response.end("Registro éxitoso");
+      setAnimes(request, response);
     }
+
     if (pathName == "/editar-animes" && method == "PUT") {
-      response.end("Editar animes");
+        updateAnimes(id_anime, request, response)
     }
     if (pathName == "/eliminar-anime" && method == "DELETE") {
-      response.end("Eliminar anime");
+      deleteAnimes(id_anime, response)
     }
   })
   .listen(port, () => {
